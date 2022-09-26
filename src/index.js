88595b9bf8f0fd4186a27bcc72f3f44db7a4eb7c
Reidond <reidond@gmail.com>
2022-09-26 16:33:24 +0300

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
 const SEND_GUS_MODAL = document.getElementById('send-gus-modal');
-const SEND_GUS_MODAL_STEP1 = document.getElementById('send-gus-modal-step-1');
-const SEND_GUS_MODAL_STEP2 = document.getElementById('send-gus-modal-step-2');
+const SEND_GUS_MODAL_FORM = document.getElementById('send-gus-modal-form');
+const SUCCESS_GUS_MODAL = document.getElementById('success-gus-modal');
 const OPEN_GUS_MODAL = document.getElementById('open-gus-modal');
 
-function hideElement(el, value = true) {
-  if (value) {
-    el.classList.remove('hidden');
-    el.classList.add('hidden');
-    el.dataset.hidden = 'true';
-  } else {
-    el.classList.remove('hidden');
-    el.dataset.hidden = 'false';
-  }
-}
-
-function resetModalState() {
-  hideElement(SEND_GUS_MODAL_STEP1, false);
-  hideElement(SEND_GUS_MODAL_STEP2);
-}
-
-SEND_GUS_MODAL.addEventListener('close', (e) => {
-  const returnValue = SEND_GUS_MODAL.returnValue;
-  if (returnValue === 'close') {
-    resetModalState();
-  } else if (returnValue === 'ok') {
-    SEND_GUS_MODAL.showModal();
-    hideElement(SEND_GUS_MODAL_STEP1, true);
-    hideElement(SEND_GUS_MODAL_STEP2, false);
-  }
-});
-
-OPEN_GUS_MODAL.addEventListener('click', () => {
-  if (typeof SEND_GUS_MODAL.showModal === 'function') {
-    SEND_GUS_MODAL.showModal();
+function showModal(el) {
+  if (typeof el.showModal === 'function') {
+    el.showModal();
   } else {
     alert('Sorry, the <dialog> API is not supported by this browser.');
   }
+}
+
+function closeModal(el, returnValue) {
+  if (typeof el.close === 'function') {
+    el.close(returnValue);
+  } else {
+    alert('Sorry, the <dialog> API is not supported by this browser.');
+  }
+}
+
+OPEN_GUS_MODAL.addEventListener('click', () => {
+  showModal(SEND_GUS_MODAL);
+});
+
+SEND_GUS_MODAL_FORM.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (SEND_GUS_MODAL.returnValue === 'close') {
+    return;
+  }
+
+  const formData = new FormData(SEND_GUS_MODAL_FORM);
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams(formData).toString(),
+  }).then(() => {
+    closeModal(SEND_GUS_MODAL, 'close');
+    showModal(SUCCESS_GUS_MODAL);
+  });
 });
